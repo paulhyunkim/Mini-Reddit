5ceb649db0991f50d654edd584dbe0a0f75ff415
Paul Kim <paulhyunkim@gmail.com>
2014-11-21 14:23:23 -0800

    moved posts from controller to factory

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 angular.module('flapperNews', [])
-  .controller('MainCtrl', ['$scope', function($scope) {
+  .factory('posts', [function(){
+    var o = {
+      posts: []
+    };
+    return o;
+  }])
+  .controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
     $scope.test = 'Hello world!';
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
-    ];
+    $scope.posts = posts.posts;
 
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
       $scope.title = '';
+      $scope.link = '';
     };
 
     $scope.incrementUpvotes = function(post) {
