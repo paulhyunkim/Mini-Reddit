d0203ec6c5d111578e1a9915e7df3442a7cc1542
Paul Kim <paulhyunkim@gmail.com>
2014-11-21 14:35:03 -0800

    added PostCtrl

--- app.js
+++ app.js
@@ -1,36 +1,52 @@
 angular.module('flapperNews', ['ui.router'])
+
   .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
       $stateProvider
         .state('home', {
           url: '/home',
           templateUrl: '/home.html',
           controller: 'MainCtrl'
+        })
+        .state('posts', {
+          url: '/posts/{id}',
+          templateUrl: '/posts.html',
+          controller: 'PostsCtrl'
         });
       $urlRouterProvider.otherwise('home');
     }])
+
   .factory('posts', [function(){
     var o = {
       posts: []
     };
     return o;
   }])
-  .controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
-    $scope.test = 'Hello world!';
-    $scope.posts = posts.posts;
 
-    $scope.addPost = function(){
-      if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({
-        title: $scope.title,
-        link: $scope.link,
-        upvotes: 0
-      });
-      $scope.title = '';
-      $scope.link = '';
-    };
+  .controller('MainCtrl', ['$scope', 'posts', 
+    function($scope, posts) {
+      $scope.test = 'Hello world!';
+      $scope.posts = posts.posts;
 
-    $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
-    };
-  }]);
+      $scope.addPost = function(){
+        if(!$scope.title || $scope.title === '') { return; }
+        $scope.posts.push({
+          title: $scope.title,
+          link: $scope.link,
+          upvotes: 0
+        });
+        $scope.title = '';
+        $scope.link = '';
+      };
+
+      $scope.incrementUpvotes = function(post) {
+        post.upvotes += 1;
+      };
+    }
+  ])
+
+  .controller('PostsCtrl', ['$scope', '$stateParams', 'posts',
+    function($scope, $stateParams, posts){
+
+    }
+  ]);
 
