2262c35c1abe6fe8fc9140f802defbcf45d91808
Paul Kim <paulhyunkim@gmail.com>
2014-11-21 16:08:50 -0800

    tying backend to frontend; retrieving and saving posts to mongo

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -5,7 +5,12 @@ angular.module('flapperNews', ['ui.router'])
         .state('home', {
           url: '/home',
           templateUrl: '/home.html',
-          controller: 'MainCtrl'
+          controller: 'MainCtrl',
+          resolve: {
+            postPromise: ['posts', function(posts){
+              return posts.getAll();
+            }]
+          }
         })
         .state('posts', {
           url: '/posts/{id}',
@@ -15,10 +20,28 @@ angular.module('flapperNews', ['ui.router'])
       $urlRouterProvider.otherwise('home');
     }])
 
-  .factory('posts', [function(){
+  .factory('posts', ['$http', function($http) {
     var o = {
       posts: []
     };
+
+    o.getAll = function() {
+      return $http.get('/posts').success(function(data){
+        angular.copy(data, o.posts);
+      });
+    };
+    o.create = function(post) {
+      return $http.post('/posts', post).success(function(data){
+        o.posts.push(data);
+      });
+    };
+    o.upvote = function(post) {
+      return $http.put('/posts/' + post._id + '/upvote')
+        .success(function(data){
+          post.upvotes += 1;
+        });
+    };
+
     return o;
   }])
 
@@ -29,19 +52,16 @@ angular.module('flapperNews', ['ui.router'])
 
       $scope.addPost = function(){
         if(!$scope.title || $scope.title === '') { return; }
-        $scope.posts.push({
+        posts.create({
           title: $scope.title,
           link: $scope.link,
-          upvotes: 0,
-          comments: [
-            {author: 'Joe', body: 'Cool post!', upvotes: 0},
-            {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-          ]
         });
+        $scope.title = '';
+        $scope.link = '';
       };
 
       $scope.incrementUpvotes = function(post) {
-        post.upvotes += 1;
+        posts.upvote(post);
       };
     }
   ])
--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -43,8 +43,22 @@ router.param('post', function(req, res, next, id) {
   });
 });
 
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error("can't find comment")); }
+
+    req.comment = comment;
+    return next();
+  });
+});
+
 router.get('/posts/:post', function(req, res) {
-  res.json(req.post);
+  req.post.populate('comments', function(err, post) {
+    res.json(post);
+  });
 });
 
 router.put('/posts/:post/upvote', function(req, res, next) {
@@ -52,5 +66,29 @@ router.put('/posts/:post/upvote', function(req, res, next) {
     if (err) { return next(err); }
 
     res.json(post);
+  });
+});
+
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.post.upvote(function(err, comment){
+    if (err) { return next(err); }
+
+    res.json(comment);
   });
 });
